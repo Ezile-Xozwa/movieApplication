3f8f124b1e426e1acab794f8cc30b07a746d811d
Ezile-Xozwa <164625605+Ezile-Xozwa@users.noreply.github.com>
2025-03-28 23:20:22 +0200

    Update BookingFactory.java

--- src/main/java/za/ac/cput/factory/BookingFactory.java
+++ src/main/java/za/ac/cput/factory/BookingFactory.java
@@ -1,4 +1,19 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.*;
+
+import za.ac.cput.domain.Booking;
+import za.ac.cput.util.BookingGenericHelper;
+
 public class BookingFactory {
+
+    public static Booking createBooking(int bookingId, User user, Showtime showtime, Seat seat, BookingStatus status) {
+        return new Booking.Builder()
+                .setBookingId(bookingId)
+                .setUser(user)
+                .setShowtime(showtime)
+                .setSeat(seat)
+                .setStatus(status)
+                .build();
+    }
 }
