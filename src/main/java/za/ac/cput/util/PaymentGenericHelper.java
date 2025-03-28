c5c8a04a3a2b49d0fadaf9751929e45f66e22666
Katie Tolo <129679232+pitsi-Dolo@users.noreply.github.com>
2025-03-28 23:24:20 +0200

    Update PaymentGenericHelper

--- src/main/java/za/ac/cput/util/PaymentGenericHelper.java
+++ src/main/java/za/ac/cput/util/PaymentGenericHelper.java
@@ -1,4 +1,10 @@
 package za.ac.cput.util;
 
+import java.util.UUID;
+
 public class PaymentGenericHelper {
+
+    public static String generateID() {
+        return UUID.randomUUID().toString();
+    }
 }
