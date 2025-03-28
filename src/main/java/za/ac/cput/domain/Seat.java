a37ee32dc16aa044c70415b10d28046b4c67c1ed
ezile <221321101@mycput.ac.za>
2025-03-28 17:53:24 +0200

    Add

--- src/main/java/za/ac/cput/domain/Seat.java
+++ src/main/java/za/ac/cput/domain/Seat.java
@@ -1,4 +1,85 @@
 package za.ac.cput.domain;
 
 public class Seat {
+
+    private String seatId;
+    private char row;
+    private int seatNumber;
+    private Status status;
+    public enum Status{Available, Booked};
+
+    private Seat(){
+
+    }
+
+    private Seat(Builder builder){
+        this.seatId = builder.seatId;
+        this.row = builder.row;
+        this.seatNumber = builder.seatNumber;
+        this.status = builder.status;
+    }
+
+    public String getSeatId(){
+        return seatId;
+    }
+
+    public char getRow(){
+        return row;
+    }
+
+    public int getSeatNumber(){
+        return seatNumber;
+    }
+
+    public Status getStatus(){
+        return status;
+    }
+
+    @Override
+    public String toString(){
+        return "Seat ID: " + seatId + "\n"
+                + "Row: " + row + "\n"
+                + "Seat Number: " + seatNumber + "\n"
+                + "Status: " + status + "\n";
+    }
+
+    public static class Builder {
+        private String seatId;
+        private char row;
+        private int seatNumber;
+        private Status status = Status.Available;
+
+        public Builder setSeatId(String seatId) {
+            this.seatId = seatId;
+            return this;
+        }
+
+        public Builder setRow(char row) {
+            this.row = row;
+            return this;
+        }
+
+        public Builder setSeatNumber(int seatNumber) {
+            this.seatNumber = seatNumber;
+            return this;
+        }
+
+        public Builder setStatus(Status status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder copy(Seat seat) {
+            this.seatId = seat.seatId;
+            this.row = seat.row;
+            this.seatNumber = seat.seatNumber;
+            this.status = seat.status;
+            return this;
+        }
+
+        public Seat build(){
+            return new Seat(this);
+        }
+    }
 }
+
