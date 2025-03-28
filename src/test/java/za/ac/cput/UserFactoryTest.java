f15abe412999e3cb11d5d8def8c24106faae37e6
anongxa <61239936+anongxa@users.noreply.github.com>
2025-03-28 20:40:11 +0200

    Update UserFactoryTest.java

--- src/test/java/za/ac/cput/UserFactoryTest.java
+++ src/test/java/za/ac/cput/UserFactoryTest.java
@@ -1,4 +1,18 @@
 package za.ac.cput;
 
+import za.ac.cput.domain.User;
+import za.ac.cput.factory.UserFactory;
+import za.ac.cput.util.UserGenericHelper;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class UserFactoryTest {
+
+    @Test
+    void createUser() {
+
+    }
+
+    private void assertNotNull(User user) {
+    }
 }
